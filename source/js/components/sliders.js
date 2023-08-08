3c1867874a32dc5bff2756cb91f45a13ce662038
Roman <romanshliakhov@gmail.com>
2023-08-09 00:37:54 +0300

    add loop for slider

--- source/js/components/sliders.js
+++ source/js/components/sliders.js
@@ -3,7 +3,12 @@
 let swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   loop: true,
+  observer: true,
   effect: "fade",
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".info__swiper-pagination",
     clickable: true,
