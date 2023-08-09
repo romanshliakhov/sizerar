092a40e92a034d7ef4af1d6c48d2a211b18016e8
Roman <romanshliakhov@gmail.com>
2023-08-09 09:36:25 +0300

    add info slider

--- source/js/components/sliders.js
+++ source/js/components/sliders.js
@@ -1,19 +1,61 @@
 // import Swiper from '../vendor/swiper.js';
 
-let swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  loop: true,
-  observer: true,
-  effect: "fade",
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".info__swiper-pagination",
-    clickable: true,
-  },
-});
+// let swiper = new Swiper(".mySwiper", {
+//   spaceBetween: 30,
+//   loop: true,
+//   observer: true,
+//   effect: "fade",
+//   autoplay: {
+//     delay: 4000,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".info__swiper-pagination",
+//     clickable: true,
+//   },
+// });
 
+
+for (const newsSlider of document.querySelectorAll('.mySwiper')) {
+  if (newsSlider) {
+    (function () {
+      "use strict";
+
+      const breakpoint = window.matchMedia("(max-width:575px)");
+      let slider;
+
+      const enableSwiper = function () {
+        slider = new Swiper(newsSlider, {
+          slidesPerView: 1,
+          observer: true,
+          observeParents: true,
+          loop: true,
+          effect: "fade",
+          autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+          },
+          pagination: {
+            el: ".info__swiper-pagination",
+            clickable: true,
+          },
+        });
+      };
+
+      const breakpointChecker = function () {
+        if (breakpoint.matches === true) {
+          if (slider !== undefined) slider.destroy(true, true);
+
+          return;
+        } else if (breakpoint.matches === false) {
+          return enableSwiper();
+        }
+      };
+
+      breakpoint.addListener(breakpointChecker);
+      breakpointChecker();
+    })();
+  }
+}
 
 
